fe861135160437186c333696949ee60b2e428d9e
Michael Wilk <mjwilk25@gmail.com>
2024-10-04 15:43:49 -0400

    AdvantageKit Installation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,13 +4,23 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.rlog.RLOGServer;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
@@ -19,6 +29,31 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
+    // MJW: Advantage Kit Addition 10/4/2024
+    Logger.recordMetadata("3467", "BlackCherry");
+
+    switch (Constants.currentMode) {
+      case REAL:
+      // Running on a real robot, log to a USB stick ("/U/logs")
+      Logger.addDataReceiver(new WPILOGWriter());
+      Logger.addDataReceiver(new RLOGServer());
+      break;
+
+      case SIM:
+      // Running a physics simulator, log to NT
+      Logger.addDataReceiver(new RLOGServer());
+      break;
+
+      case REPLAY:
+      // Replaying a log, set up replay source
+      setUseTiming(false); // Run as fast as possible
+      String logPath = LogFileUtil.findReplayLog();
+      Logger.setReplaySource(new WPILOGReader(logPath));
+      Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim"), 0.01));
+    }
+
+    Logger.start();
+    // MJW: Advantage Kit Addition 10/4/2024
     m_robotContainer = new RobotContainer();
 
     m_robotContainer.drivetrain.getDaqThread().setThreadPriority(99);
