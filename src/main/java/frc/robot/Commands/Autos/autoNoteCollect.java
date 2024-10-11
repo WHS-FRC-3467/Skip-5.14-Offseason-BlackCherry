2db3854be4153e31576cd4602f7ebc0ca62a1a51
aaravVid <vidyarthyaarav@gmail.com>
2024-10-10 20:49:32 -0400

    auto commands from scratch

---
+++ src/main/java/frc/robot/Commands/Autos/autoNoteCollect.java
@@ -0,0 +1,13 @@
+package frc.robot.Commands.Autos;
+
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.IntakeJoint;
+import frc.robot.subsystems.IntakeRollers;
+
+
+public class autoNoteCollect {
+    
+}
